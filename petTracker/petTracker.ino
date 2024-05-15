1dd9b43eb11488484e326b12d819d9a844aa0ce3
Mehmet BULUT <mehmetbulut4063@outlook.com>
2024-05-15 19:43:40 +0300

    Camera module defined

--- petTracker/petTracker.ino
+++ petTracker/petTracker.ino
@@ -4,6 +4,11 @@
 #include <Arduino.h>
 #include <Firebase_ESP_Client.h>
 #include "WiFi.h"
+#include "esp_camera.h"
+#include "soc/soc.h"           // Disable brownout problems
+#include "soc/rtc_cntl_reg.h"  // Disable brownout problems
+#include "driver/rtc_io.h"
+#include <LittleFS.h>
 //Tokens for RTDB connection
 #include "addons/TokenHelper.h"
 #include "addons/RTDBHelper.h"
@@ -12,8 +17,7 @@
 
 //Network credentials
 const char* ssid = "El Gato(2.4)";
-const char* password = "";
-
+const char* password = ""
 //Firebase project api key
 #define API_KEY ""
 
@@ -26,6 +30,27 @@ const char* password = "";
 
 //Database url
 #define DATABASE_URL "https://pet-tracker-e6c45-default-rtdb.europe-west1.firebasedatabase.app/"
+//Data path for photo
+#define FILE_PHOTO_PATH "/photo.jpg"
+#define BUCKET_PHOTO "/data/photo.jpg"
+
+// Camera pins according to my ESP32-Cam model(CAMERA_MODEL_AI_THINKER)
+#define PWDN_GPIO_NUM     32
+#define RESET_GPIO_NUM    -1
+#define XCLK_GPIO_NUM      0
+#define SIOD_GPIO_NUM     26
+#define SIOC_GPIO_NUM     27
+#define Y9_GPIO_NUM       35
+#define Y8_GPIO_NUM       34
+#define Y7_GPIO_NUM       39
+#define Y6_GPIO_NUM       36
+#define Y5_GPIO_NUM       21
+#define Y4_GPIO_NUM       19
+#define Y3_GPIO_NUM       18
+#define Y2_GPIO_NUM        5
+#define VSYNC_GPIO_NUM    25
+#define HREF_GPIO_NUM     23
+#define PCLK_GPIO_NUM     22
 
 //Firebase data objects
 FirebaseData fbdo;
